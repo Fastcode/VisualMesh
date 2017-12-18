2af676bd1809a8f51a6f25ed5707afbae67fba65
Trent Houliston <trent@houliston.me>
2017-12-18 16:15:57 +1100

    Changes

--- tf.py
+++ tf.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 import numpy as np
 import gzip
 import struct
+from PIL import Image, ImageDraw
 import os
-import re
 
 
 def load_pack(file):
@@ -35,10 +35,10 @@ def load_pack(file):
             ci, cv = struct.unpack('<If', d1)
 
             # Graph data
-            graph = np.frombuffer(f.read(200 * 4 * 7), np.int32)
+            graph = np.frombuffer(f.read(200 * 4 * 7), np.int32).reshape(-1, 7, order='C')
 
             # Colour data
-            colours = np.frombuffer(f.read(200 * 4 * 3), np.float32).reshape(3, -1, order='F')
+            colours = np.frombuffer(f.read(200 * 4 * 3), np.float32).reshape(-1, 3, order='C')
             stencil = np.frombuffer(f.read(200 * 4 * 1), np.float32)
 
             # Append our various datas to the list
@@ -51,8 +51,53 @@ def load_pack(file):
     return np.array(X), np.array(Y), np.array(Yi), np.array(G)
 
 
+def load_validation(file):
+
+    # THe output images
+    output = []
+
+    # Open the file
+    with gzip.open(file, 'rb') as f:
+
+        while True:
+
+            # Try to read 8 bytes and end when there are none
+            d1 = f.read(8)
+            if len(d1) < 8:
+                break
+
+            # The file number and the size of this mesh
+            file_no, size = struct.unpack('<II', d1)
+
+            # Graph data (7 values per point)
+            graph = np.frombuffer(f.read(size * 4 * 7), np.int32).reshape(-1, 7, order='C').astype(np.float32)
+
+            # Pixel coordinates (2 values per point)
+            coords = np.frombuffer(f.read(size * 4 * 2), np.int32).reshape(-1, 2, order='C')
+
+            # Colour data (3 values per point)
+            colours = np.frombuffer(f.read(size * 4 * 3), np.float32).reshape(-1, 3, order='C')
+
+            # Stencil data
+            stencil = np.frombuffer(f.read(size * 4 * 1), np.float32)
+
+            output.append({
+                'file_no': file_no,
+                'graph': graph,
+                'coords': coords,
+                'colours': colours,
+                'stencil': stencil
+            })
+
+    # Squash the results into numpy arrays and return
+    return output
+
+
 # Learning rate
 learning_rate = 0.001
+
+# Batch size
+batch_size = 1000
 
 # Number of classes in the final output
 n_classes = 2  # 2 classes, ball and not ball
@@ -63,52 +108,100 @@ graph_degree = 7
 # Each number is output neurons for that layer, each list in the list is a convolution
 groups = [[5, 3], [5, 3], [5, 3], [5, n_classes]]
 
+# The size of the mesh graph
+Gs = tf.placeholder(dtype=tf.int32, name='MeshSize')
+
 # The mesh graph
-G = tf.placeholder(tf.int32, [None, 200 * graph_degree], name="MeshGraph")
-onehot_G = tf.one_hot(G, 200)
+G = tf.placeholder(dtype=tf.int32, shape=[None, None, graph_degree], name='MeshGraph')
+
 
 # First input is the number of visual mesh points by 3
-X = tf.placeholder(tf.float32, [None, 3, 200], name="Input")
+X = tf.placeholder(dtype=tf.float32, shape=[None, None, 3], name='Input')
 
 # The final expected output for the classes
-Y = tf.placeholder(tf.float32, [None, 2], name="Output")
+Y = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='Output')
 
 # The index of the final output we are checking
-Yi = tf.placeholder(tf.int32, [None, 1], name="OutputIndex")
-
+Yi = tf.placeholder(dtype=tf.int32, shape=[None, 1], name='OutputIndex')
 
 # Build our tensor
-logits = X
-count = 0
-for p, c in zip([[X.get_shape()[1].value]] + groups, groups):
-    count += 1
-    prev_last_out = p[-1]
+network = X
+for i, c in enumerate(groups):
+    # Which convolution we are on
+    with tf.name_scope('Conv_{}'.format(i)):
 
-    # Gather the relevant rows and reshape to stack them
-    logits = tf.einsum('ajk,aik->aij', logits, onehot_G)
-    logits = tf.reshape(logits, [-1, prev_last_out * graph_degree, 200], name="Reshape" + str(count))
+        # The size of the previous output is the size of our input
+        prev_last_out = network.get_shape()[2].value
 
-    for in_s, out_s in zip([prev_last_out * graph_degree] + c, c):
-        # Apply weights and biases
-        W = tf.Variable(tf.random_normal([in_s, out_s]))
-        b = tf.Variable(tf.random_normal([out_s]))
+        with tf.name_scope('GatherConvolution'):
 
-        logits = tf.matrix_transpose(tf.tensordot(logits, W, [[1], [0]], name="MatMul1") + b)
-        # Apply our activation function
-        logits = tf.nn.elu(logits)
+            if False:
+                # Construct a 4D tensor of size [?, 200, graph_degree * prev_last_output, 3] filled with a grid of indices
+                net_shape = tf.shape(network)
+                idx_a, idx_b, idx_c = tf.meshgrid(tf.range(net_shape[0]),
+                                                  tf.range(net_shape[1]),
+                                                  tf.range(graph_degree * prev_last_out),
+                                                  indexing='ij',
+                                                  name='Indices')
 
-logits = tf.nn.softmax(logits, dim=1)
-logits = tf.einsum('ajk,aik->aj', logits, tf.one_hot(Yi, 200))
+                # Use these indexes to lookup our graph indices
+                # TODO note we have to hack using bitcast as tensorflow won't gather int32s on the GPU FOR NO RAISIN
+                idx_b = tf.bitcast(tf.gather_nd(tf.bitcast(G, type=tf.float32), tf.stack([idx_a, idx_b, tf.div(idx_c, graph_degree)], axis=3), name='GraphIndices'), type=tf.int32)
 
-with tf.name_scope('Loss'):
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+                # Now we can use this to lookup our actual network
+                network = tf.gather_nd(network, tf.stack([idx_a, idx_b, tf.mod(idx_c, prev_last_out)], axis=3), name='NetworkIndices')
 
-with tf.name_scope('Optimiser'):
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+            else:
+                # create a [None,200,graph_degree,200] tensor of one-hot vectors to select the correct indices of G
+                graph_indices = tf.one_hot(G, Gs)
+                network = tf.einsum("ijk,ibaj->ibak", network, graph_indices)
+                network = tf.reshape(network, [-1, 200, prev_last_out * graph_degree])
 
-with tf.name_scope('Accuracy'):
-    acc = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
-    acc = tf.reduce_mean(tf.cast(acc, tf.float32))
+        for j, out_s in enumerate(c):
+
+            # Our input size is the previous output size
+            in_s = network.get_shape()[2].value
+
+            # Which layer we are on
+            with tf.name_scope('Layer_{}'.format(j)):
+
+                # Create weights and biases
+                W = tf.Variable(tf.truncated_normal(shape=[in_s, out_s], mean=0.0, stddev=np.sqrt(in_s)), dtype=tf.float32, name='Weights')
+                b = tf.Variable(tf.truncated_normal(shape=[out_s], mean=0.0, stddev=np.sqrt(out_s)), dtype=tf.float32, name='Biases')
+
+                if False:
+                    # Multiply each slice by the weights matrix
+                    network = tf.einsum('ijk,kl->ijl', network, W)
+
+                    # Add the bias
+                    network = tf.add(network, b)
+                else:
+                    network = tf.tensordot(network, W, [[2], [0]], name="MatMul1") + b
+
+                # Apply our activation function
+                network = tf.nn.elu(network)
+
+# Softmax our final output for all values in the mesh
+network = tf.nn.softmax(network, name='Softmax')
+
+# Gather our individual output for training
+with tf.name_scope('Training'):
+    training_indices = tf.one_hot(indices=Yi, depth=Gs, dtype=tf.float32, name='OneHotTrainingIndices')
+    train_logits = tf.einsum('ijk,ibj->ibk', network, training_indices)
+    train_logits = tf.reshape(train_logits, [-1, 2])
+
+    # Our loss function
+    with tf.name_scope('Loss'):
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=train_logits, labels=Y))
+
+    # Our optimiser
+    with tf.name_scope('Optimiser'):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # Calculate accuracy
+    with tf.name_scope('Accuracy'):
+        acc = tf.equal(tf.argmax(train_logits, 1), tf.argmax(Y, 1))
+        acc = tf.reduce_mean(tf.cast(acc, tf.float32))
 
 # Monitor cost and accuracy tensor
 tf.summary.scalar('loss', cost)
@@ -121,9 +214,10 @@ for var in tf.trainable_variables():
 # Merge all summaries into a single op
 merged_summary_op = tf.summary.merge_all()
 
-with tf.Session() as sess:
+config = tf.ConfigProto()
+config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+with tf.Session(config=config) as sess:
 
-    batch_size = 1000
     training_epochs = 10
 
     # Initialise the variables
@@ -137,17 +231,20 @@ with tf.Session() as sess:
     packs = sorted([os.path.join(pack_dir, f) for f in os.listdir(pack_dir) if f.endswith('.bin.gz')])
 
     # First pack will be used as validation data
-    print('Loading pack {} as validation'.format(packs[0]))
-    validation = load_pack(packs[0])
+    validation_dir = os.path.join('training', 'validation')
+    validation_pack = os.path.join(validation_dir, 'validation.bin.gz')
+    print('Loading validation pack')
+    validation = load_validation(validation_pack)
     print('\tloaded')
 
     training_samples = 0
+    output_file_no = 0
 
     # The number of epochs to train
     for epoch in range(training_epochs):
 
         # The rest of the packs for training
-        for pack in packs[1:]:
+        for pack in packs:
 
             # Load the pack
             print('Loading data pack {}'.format(pack))
@@ -159,6 +256,7 @@ with tf.Session() as sess:
 
                 # Run our training step
                 _, c, summary = sess.run([optimizer, cost, merged_summary_op], feed_dict={
+                    Gs: 200,
                     X: data[0][i:i + batch_size],
                     Y: data[1][i:i + batch_size],
                     Yi: np.array(data[2][i:i + batch_size]).reshape((-1, 1)),
@@ -168,3 +266,48 @@ with tf.Session() as sess:
                 # Write summary log
                 training_samples += len(data[0][i:i + batch_size])
                 summary_writer.add_summary(summary, training_samples)
+
+            continue
+
+            # Run the network after each pack file for our example images
+            output_file_no += 1
+            for v in validation:
+
+                # Run our network for this validation object
+                result = sess.run([network], feed_dict={
+                    Gs: len(v['colours']),
+                    X: [v['colours']],
+                    G: [v['graph']]
+                })
+
+                # Load our input image
+                img = Image.open(os.path.join(validation_dir, 'image{:07d}.png'.format(v['file_no'] - 1)))
+                d = ImageDraw.Draw(img)
+
+                # Go through our drawing coordinates
+                for i in range(len(v['coords'])):
+
+                    # The result at our point
+                    r1 = result[0][0][i]
+
+                    # The pixel coordinates at our point
+                    p1 = v['coords'][i]
+
+                    # Go through our neighbours
+                    for n in v['graph'][i]:
+
+                        # The result at our neighbours point
+                        r2 = result[0][0][i]
+
+                        # The coordinate at our neighbours point
+                        p2 = v['coords'][n]
+
+                        # Draw a line if both are in the image
+                        if p2[0] != -1 and p2[1] != -1:
+                            r = max(min(int(round(r1[1] * 255)), 255), 0)
+                            g = max(min(int(round(r1[0] * 255)), 255), 0)
+
+                            d.line([(p1[0], p1[1]), (p2[0], p2[1])], fill=(r, g, 0, 64))
+
+                # Save our image
+                img.save(os.path.join('output', 'validation', '{:04d}_{:04d}.png'.format(v['file_no'], output_file_no)))
