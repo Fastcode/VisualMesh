d507ff95d553e4d8c14c9ad2284636fa1557e96a
Trent Houliston <trent@houliston.me>
2017-12-20 19:11:43 +1100

    Add resampling code

--- tf.py
+++ tf.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+import os
+import gzip
+import struct
+import math
+import collections
+import random
+
 import tensorflow as tf
+import numpy as np
+from PIL import Image, ImageDraw
 
 import learning.load as load
-from PIL import Image, ImageDraw
-import os
-import math
+
 
 # Learning rate
 learning_rate = 0.001
@@ -150,27 +157,11 @@ init_op = tf.global_variables_initializer()
 # We save our trainable variables
 saver = tf.train.Saver({v.name: v for v in tf.trainable_variables()})
 
-config = tf.ConfigProto()
-config.graph_options.build_cost_model = 1
-config.gpu_options.per_process_gpu_memory_fraction = 0.9
-config.gpu_options.allow_growth = True
-config.gpu_options.force_gpu_compatible = True
-
 run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 run_metadata = tf.RunMetadata()
 
-with tf.Session(config=config) as sess:
-
-    # Initialise variables
-    sess.run(init_op)
-
-    # Checkpoint path
-    model_path = os.path.join('training', 'model.ckpt')
-
-    # Load model or not
-    load_model = True
-    if load_model:
-        saver.restore(sess, model_path)
+# Train the network
+def train(sess):
 
     # Setup for tensorboard
     summary_writer = tf.summary.FileWriter('logs', graph=tf.get_default_graph())
@@ -265,3 +256,126 @@ with tf.Session(config=config) as sess:
 
                     # Save our image
                     img.save(os.path.join('output', 'validation', '{:04d}_{:04d}.png'.format(v['file_no'] - 1, output_file_no)))
+
+
+def get_tree_from_point(mesh, index):
+
+    # Get our parts
+    colour = mesh[0]
+    stencil = mesh[1]
+    graph = mesh[2]
+
+    # Some constants
+    n_points = 200
+
+    # Get 200 points centred around our input index
+    queue = collections.deque([0, index])
+    points = set()
+
+    # We only actually gather 199, since 0 is the null point
+    while len(queue) > 0 and len(points) < n_points:
+        # Get our next element and add it to our points
+        i = queue.popleft()
+        points.add(i)
+
+        # Add our points to the queue but don't bother checking the first one (that's us)
+        for n in graph[i][1:]:
+            if n not in points and n not in queue:
+                queue.append(n)
+
+    # Sort and make into a list
+    points = np.array(sorted(list(points)), dtype=np.int32)
+
+    # Make our reverse lookup map
+    r_indices = np.zeros(len(stencil), dtype=np.int32)
+    for i, v in enumerate(points):
+        r_indices[v] = i
+
+    # Gather our new data
+    new_graph = np.take(r_indices, np.take(graph, points, axis=0))
+    new_colour = np.take(colour, points, axis=0)
+    new_stencil = np.take(stencil, points, axis=0)
+
+    # Return what we learnt
+    return r_indices[index], new_graph, new_colour, new_stencil
+
+def rejection_resample(sess):
+
+    # Minimum chance of being selected is 5%
+    min_chance = 0.05
+
+    # Get our data packs
+    pack_dir = os.path.join('training', 'packs')
+    repack_dir = os.path.join('training', 'resample')
+    packs = sorted([f for f in os.listdir(pack_dir) if f.endswith('.bin.gz')])
+
+    for pack in packs[32::4]:
+        print('Loading data tree pack {}'.format(pack))
+        data = load.pack(os.path.join(pack_dir, pack))
+        print('\tloaded')
+
+        samples = []
+
+        for in_X, in_Y, in_G in zip(*data):
+
+            # Run our network for this input object
+            result, = sess.run([network], feed_dict={
+                X: [in_X],
+                G: [in_G]
+            })
+
+            # We are not running batches so this is always 1 element
+            result = result[0]
+
+            # Now we must select all the ball points
+            bp, = np.nonzero(in_Y)
+
+            # Now get the probabilities of selecting each point
+            # The more wrong the values are the more likely they are to be selected
+            probs = np.abs(result[:,0] - in_Y) + (min_chance / (len(in_Y) - len(bp)))
+
+            # We will never reselect our ball points so set their probability to 0
+            np.put(probs, bp, 0.0)
+
+            # Normalise our probabilities so they sum to 1
+            probs = probs / np.sum(probs)
+
+            # Randomly select n points
+            nbp = np.random.choice(a=len(probs), size=len(bp), p=probs, replace=False)
+
+            # Add them to the list
+            for p in bp.tolist() + nbp.tolist():
+                samples.append(get_tree_from_point((in_X, in_Y, in_G), p))
+
+        # Save our samples in a batch
+        with open(os.path.join(repack_dir, pack[:-3]), 'wb') as f:
+            # Write out the data in a random order
+            random.shuffle(samples)
+            for i, new_graph, new_colour, new_stencil in samples:
+
+                f.write(struct.pack('<If', i, new_stencil[i]))
+                f.write(new_graph.tobytes())
+                f.write(new_colour.tobytes())
+                f.write(new_stencil.tobytes())
+
+config = tf.ConfigProto()
+config.graph_options.build_cost_model = 1
+config.gpu_options.per_process_gpu_memory_fraction = 0.2
+config.gpu_options.allow_growth = True
+config.gpu_options.force_gpu_compatible = True
+
+with tf.Session(config=config) as sess:
+
+    # Initialise variables
+    sess.run(init_op)
+
+    # Checkpoint path
+    model_path = os.path.join('training', 'model.ckpt')
+
+    # Load model or not
+    load_model = True
+    if load_model:
+        saver.restore(sess, model_path)
+
+    # train(sess)
+    rejection_resample(sess)
