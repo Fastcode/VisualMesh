a62fe9f0efd7eb0ce23bc8c0a6814afdb7295970
Trent Houliston <trent@houliston.me>
2019-04-10 11:22:40 +1000

    Fix calculation for sphere index

--- src/geometry/Sphere.hpp
+++ src/geometry/Sphere.hpp
@@ -85,10 +85,10 @@ namespace geometry {
       }
 
       // Valid below the horizon
-      if (h > 0 && phi < M_PI_2) { return 2 * std::asin(r / ((h-r) * std::tan(phi)) / k; }
+      if (h > 0 && phi < M_PI_2) { return 2 * std::asin(r / ((h - r) * std::tan(phi))) / k; }
       // Valid above the horizon
       else if (h < 0 && phi > M_PI_2) {
-        return 2 * std::asin(r / (-(h-r) * std::tan(M_PI - phi))) / k;
+        return 2 * std::asin(r / (-(h - r) * std::tan(M_PI - phi))) / k;
       }
       // Other situations are invalid so return NaN
       else {
