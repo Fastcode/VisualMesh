f2160e5bb35c4e4508029831ade69696a0084bdb
Trent Houliston <trent@houliston.me>
2019-07-29 17:50:54 +1000

    Add missing 3d projection code

--- src/util/projection.hpp
+++ src/util/projection.hpp
@@ -24,7 +24,7 @@
 namespace visualmesh {
 
 template <typename Scalar>
-inline vec2<Scalar> project_equidistant(const vec4<Scalar>& p, const Lens<Scalar>& lens) {
+inline vec2<Scalar> project_equidistant(const vec3<Scalar>& p, const Lens<Scalar>& lens) {
   // Calculate some intermediates
   Scalar theta      = std::acos(p[0]);
   Scalar r          = lens.focal_length * theta;
@@ -42,16 +42,16 @@ inline vec2<Scalar> project_equidistant(const vec4<Scalar>& p, const Lens<Scalar
 }
 
 template <typename Scalar>
-inline vec4<Scalar> unproject_equidistant(const vec2<Scalar>& point, const Lens<Scalar>& lens) {
+inline vec3<Scalar> unproject_equidistant(const vec2<Scalar>& point, const Lens<Scalar>& lens) {
   vec2<Scalar> screen =
     subtract(multiply(cast<Scalar>(lens.dimensions), static_cast<Scalar>(0.5)), add(point, lens.centre));
   Scalar r     = norm(screen);
   Scalar theta = r / lens.focal_length;
-  return normalise(vec4<Scalar>{std::cos(theta), std::sin(theta) * screen[0] / r, std::sin(theta) * screen[1] / r});
+  return normalise(vec3<Scalar>{std::cos(theta), std::sin(theta) * screen[0] / r, std::sin(theta) * screen[1] / r});
 }
 
 template <typename Scalar>
-inline vec2<Scalar> project_equisolid(const vec4<Scalar>& p, const Lens<Scalar>& lens) {
+inline vec2<Scalar> project_equisolid(const vec3<Scalar>& p, const Lens<Scalar>& lens) {
   // Calculate some intermediates
   Scalar theta      = std::acos(p[0]);
   Scalar r          = static_cast<Scalar>(2.0) * lens.focal_length * std::sin(theta * static_cast<Scalar>(0.5));
@@ -69,16 +69,16 @@ inline vec2<Scalar> project_equisolid(const vec4<Scalar>& p, const Lens<Scalar>&
 }
 
 template <typename Scalar>
-inline vec4<Scalar> unproject_equisolid(const vec2<Scalar>& point, const Lens<Scalar>& lens) {
+inline vec3<Scalar> unproject_equisolid(const vec2<Scalar>& point, const Lens<Scalar>& lens) {
   vec2<Scalar> screen =
     subtract(multiply(cast<Scalar>(lens.dimensions), static_cast<Scalar>(0.5)), add(point, lens.centre));
   Scalar r     = norm(screen);
   Scalar theta = 2.0 * std::asin(r / (2.0 * lens.focal_length));
-  return normalise(vec4<Scalar>{std::cos(theta), std::sin(theta) * screen[0] / r, std::sin(theta) * screen[1] / r, 0});
+  return normalise(vec3<Scalar>{std::cos(theta), std::sin(theta) * screen[0] / r, std::sin(theta) * screen[1] / r});
 }
 
 template <typename Scalar>
-inline vec2<Scalar> project_rectilinear(const vec4<Scalar>& p, const Lens<Scalar>& lens) {
+inline vec2<Scalar> project_rectilinear(const vec3<Scalar>& p, const Lens<Scalar>& lens) {
   // Work out our pixel coordinates as a 0 centred image with x to the left and y up (screen space)
   vec2<Scalar> screen = {{lens.focal_length * p[1] / p[0], lens.focal_length * p[2] / p[0]}};
 
@@ -88,14 +88,14 @@ inline vec2<Scalar> project_rectilinear(const vec4<Scalar>& p, const Lens<Scalar
 }
 
 template <typename Scalar>
-inline vec4<Scalar> unproject_rectilinear(const vec2<Scalar>& point, const Lens<Scalar>& lens) {
+inline vec3<Scalar> unproject_rectilinear(const vec2<Scalar>& point, const Lens<Scalar>& lens) {
   vec2<Scalar> screen =
     subtract(multiply(cast<Scalar>(lens.dimensions), static_cast<Scalar>(0.5)), add(point, lens.centre));
-  return normalise(vec4<Scalar>{lens.focal_length, screen[0], screen[1], 0});
+  return normalise(vec3<Scalar>{lens.focal_length, screen[0], screen[1]});
 }
 
 template <typename Scalar>
-inline vec2<Scalar> project(const vec4<Scalar>& p, const Lens<Scalar>& lens) {
+inline vec2<Scalar> project(const vec3<Scalar>& p, const Lens<Scalar>& lens) {
   switch (lens.projection) {
     case EQUISOLID: return project_equisolid(p, lens);
     case EQUIDISTANT: return project_equidistant(p, lens);
@@ -105,7 +105,7 @@ inline vec2<Scalar> project(const vec4<Scalar>& p, const Lens<Scalar>& lens) {
 }
 
 template <typename Scalar>
-inline vec4<Scalar> unproject(const vec2<Scalar>& p, const Lens<Scalar>& lens) {
+inline vec3<Scalar> unproject(const vec2<Scalar>& p, const Lens<Scalar>& lens) {
   switch (lens.projection) {
     case EQUISOLID: return unproject_equisolid(p, lens);
     case EQUIDISTANT: return unproject_equidistant(p, lens);
