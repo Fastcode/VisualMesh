acd6d591e6161c5a52ad2ad161cebd469c068f48
Trent Houliston <trent@houliston.me>
2019-10-13 22:01:07 +1100

    Use all the GPUs

--- training/training.py
+++ training/training.py
@@ -31,55 +31,59 @@ def _prepare_dataset(args):
 # Train the network
 def train(config, output_path):
 
-  # Get the training dataset
-  training_dataset = VisualMeshDataset(
-    input_files=config.dataset.training,
-    classes=config.network.classes,
-    geometry=config.geometry,
-    batch_size=config.training.batch_size,
-    prefetch=tf.data.experimental.AUTOTUNE,
-    variants=config.training.variants,
-  ).build().map(_prepare_dataset)
+  # Use all the GPUs
+  mirrored_strategy = tf.distribute.MirroredStrategy()
+  with mirrored_strategy.scope():
 
-  # Get the validation dataset
-  validation_dataset = VisualMeshDataset(
-    input_files=config.dataset.validation,
-    classes=config.network.classes,
-    geometry=config.geometry,
-    batch_size=config.training.validation.batch_size,
-    prefetch=tf.data.experimental.AUTOTUNE,
-    variants={},
-  ).build().map(_prepare_dataset)
+    # Get the training dataset
+    training_dataset = VisualMeshDataset(
+      input_files=config.dataset.training,
+      classes=config.network.classes,
+      geometry=config.geometry,
+      batch_size=config.training.batch_size,
+      prefetch=tf.data.experimental.AUTOTUNE,
+      variants=config.training.variants,
+    ).build().map(_prepare_dataset)
 
-  # Define the model
-  model = VisualMeshModel(
-    structure=config.network.structure, n_classes=len(config.network.classes), activation=config.network.activation_fn
-  )
-  model.compile(
-    optimizer=tf.optimizers.Adam(),
-    loss=tf.losses.CategoricalCrossentropy(),
-    metrics=[
-      tf.metrics.CategoricalCrossentropy(),
-      tf.metrics.Precision(),
-      tf.metrics.Recall(),
-    ],
-  )
+    # Get the validation dataset
+    validation_dataset = VisualMeshDataset(
+      input_files=config.dataset.validation,
+      classes=config.network.classes,
+      geometry=config.geometry,
+      batch_size=config.training.validation.batch_size,
+      prefetch=tf.data.experimental.AUTOTUNE,
+      variants={},
+    ).build().map(_prepare_dataset)
 
-  # Find the latest checkpoint file
-  checkpoint_file = tf.train.latest_checkpoint(output_path)
-  if checkpoint_file is not None:
-    model.load_weights(checkpoint_file)
+    # Define the model
+    model = VisualMeshModel(
+      structure=config.network.structure, n_classes=len(config.network.classes), activation=config.network.activation_fn
+    )
+    model.compile(
+      optimizer=tf.optimizers.Adam(),
+      loss=tf.losses.CategoricalCrossentropy(),
+      metrics=[
+        tf.metrics.CategoricalCrossentropy(),
+        tf.metrics.Precision(),
+        tf.metrics.Recall(),
+      ],
+    )
 
-  # Fit the model
-  model.fit(
-    training_dataset,
-    steps_per_epoch=config.training.batches_per_epoch,
-    validation_data=validation_dataset,
-    validation_steps=config.training.validation.samples,
-    callbacks=[
-      tf.keras.callbacks.TensorBoard(log_dir=output_path, write_graph=True, write_images=True),
-      tf.keras.callbacks.ModelCheckpoint(
-        filepath=os.path.join(output_path, 'model.ckpt'), save_weights_only=True, verbose=1
-      ),
-    ]
-  )
+    # Find the latest checkpoint file
+    checkpoint_file = tf.train.latest_checkpoint(output_path)
+    if checkpoint_file is not None:
+      model.load_weights(checkpoint_file)
+
+    # Fit the model
+    model.fit(
+      training_dataset,
+      steps_per_epoch=config.training.batches_per_epoch,
+      validation_data=validation_dataset,
+      validation_steps=config.training.validation.samples,
+      callbacks=[
+        tf.keras.callbacks.TensorBoard(log_dir=output_path, write_graph=True, write_images=True),
+        tf.keras.callbacks.ModelCheckpoint(
+          filepath=os.path.join(output_path, 'model.ckpt'), save_weights_only=True, verbose=1
+        ),
+      ]
+    )
