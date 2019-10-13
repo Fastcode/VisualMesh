93dd5ba904daebf9ab0f2136f0ac640f63113a68
Trent Houliston <trent@houliston.me>
2019-10-13 19:33:22 +1100

    Of course :facepalm:

--- training/layer/graph_convolution.py
+++ training/layer/graph_convolution.py
@@ -8,8 +8,5 @@ class GraphConvolution(tf.keras.layers.Layer):
   def __init__(self):
     super(GraphConvolution, self).__init__()
 
-  def build(self):
-    super(GraphConvolution, self).build()
-
   def call(self, X, G):
     return tf.reshape(tf.gather(X, G, name='NetworkGather'), shape=[-1, tf.shape(X)[1] * tf.shape(G)[1]])
