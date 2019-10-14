d4c2509aeaa53c9cb7f5a116ca0e60f8a9092f70
Trent Houliston <trent@houliston.me>
2019-10-14 14:41:17 +1100

    Disable distribution as it's currently broken

--- training/training.py
+++ training/training.py
@@ -33,68 +33,66 @@ def _prepare_dataset(args):
 def train(config, output_path):
 
   # Use all the GPUs
-  mirrored_strategy = tf.distribute.MirroredStrategy()
-  with mirrored_strategy.scope():
+  # mirrored_strategy = tf.distribute.MirroredStrategy()
+  # with mirrored_strategy.scope():
 
-    # Get the training dataset
-    training_dataset = VisualMeshDataset(
-      input_files=config.dataset.training,
-      classes=config.network.classes,
-      geometry=config.geometry,
-      batch_size=config.training.batch_size,
-      prefetch=tf.data.experimental.AUTOTUNE,
-      variants=config.training.variants,
-    ).build().map(_prepare_dataset)
+  # Get the training dataset
+  training_dataset = VisualMeshDataset(
+    input_files=config.dataset.training,
+    classes=config.network.classes,
+    geometry=config.geometry,
+    batch_size=config.training.batch_size,
+    prefetch=tf.data.experimental.AUTOTUNE,
+    variants=config.training.variants,
+  ).build().map(_prepare_dataset)
 
-    # If we are using batches_per_epoch as a number rather than the whole dataset
-    if config.training.batches_per_epoch is not None:
-      training_dataset = training_dataset.repeat()
+  # If we are using batches_per_epoch as a number rather than the whole dataset
+  if config.training.batches_per_epoch is not None:
+    training_dataset = training_dataset.repeat()
 
-    # Get the validation dataset
-    validation_dataset = VisualMeshDataset(
-      input_files=config.dataset.validation,
-      classes=config.network.classes,
-      geometry=config.geometry,
-      batch_size=config.training.validation.batch_size,
-      prefetch=tf.data.experimental.AUTOTUNE,
-      variants={},
-    ).build().map(_prepare_dataset)
+  # Get the validation dataset
+  validation_dataset = VisualMeshDataset(
+    input_files=config.dataset.validation,
+    classes=config.network.classes,
+    geometry=config.geometry,
+    batch_size=config.training.validation.batch_size,
+    prefetch=tf.data.experimental.AUTOTUNE,
+    variants={},
+  ).build().map(_prepare_dataset)
 
-    # Build up the list of metrics we want to track
-    metrics = [tf.metrics.CategoricalCrossentropy(), tf.metrics.Precision(), tf.metrics.Recall()]
+  # Build up the list of metrics we want to track
+  metrics = [tf.metrics.CategoricalCrossentropy(), tf.metrics.Precision(), tf.metrics.Recall()]
 
-    for i, k in enumerate(config.network.classes):
-      metrics.append(ClassPrecision(i, name='{}_precision'.format(k[0])))
-      metrics.append(ClassRecall(i, name='{}_recall'.format(k[0])))
+  for i, k in enumerate(config.network.classes):
+    metrics.append(ClassPrecision(i, name='{}_precision'.format(k[0])))
+    metrics.append(ClassRecall(i, name='{}_recall'.format(k[0])))
 
-    # Define the model
-    model = VisualMeshModel(
-      structure=config.network.structure,
-      n_classes=len(config.network.classes),
-      activation=config.network.activation_fn
-    )
-    model.compile(
-      optimizer=tf.optimizers.Adam(),
-      loss=tf.losses.CategoricalCrossentropy(),
-      metrics=metrics,
-    )
+  # Define the model
+  model = VisualMeshModel(
+    structure=config.network.structure, n_classes=len(config.network.classes), activation=config.network.activation_fn
+  )
+  model.compile(
+    optimizer=tf.optimizers.Adam(),
+    loss=tf.losses.CategoricalCrossentropy(),
+    metrics=metrics,
+  )
 
-    # Find the latest checkpoint file
-    checkpoint_file = tf.train.latest_checkpoint(output_path)
-    if checkpoint_file is not None:
-      model.load_weights(checkpoint_file)
+  # Find the latest checkpoint file
+  checkpoint_file = tf.train.latest_checkpoint(output_path)
+  if checkpoint_file is not None:
+    model.load_weights(checkpoint_file)
 
-    # Fit the model
-    model.fit(
-      training_dataset,
-      epochs=config.training.epochs,
-      steps_per_epoch=config.training.batches_per_epoch,
-      validation_data=validation_dataset,
-      validation_steps=config.training.validation.samples,
-      callbacks=[
-        tf.keras.callbacks.TensorBoard(log_dir=output_path, update_freq='batch', write_graph=True, histogram_freq=1),
-        tf.keras.callbacks.ModelCheckpoint(
-          filepath=os.path.join(output_path, 'model.ckpt'), save_weights_only=True, verbose=1
-        ),
-      ]
-    )
+  # Fit the model
+  model.fit(
+    training_dataset,
+    epochs=config.training.epochs,
+    steps_per_epoch=config.training.batches_per_epoch,
+    validation_data=validation_dataset,
+    validation_steps=config.training.validation.samples,
+    callbacks=[
+      tf.keras.callbacks.TensorBoard(log_dir=output_path, update_freq='batch', write_graph=True, histogram_freq=1),
+      tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(output_path, 'model.ckpt'), save_weights_only=True, verbose=1
+      ),
+    ]
+  )
