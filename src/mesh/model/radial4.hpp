722dbeb61e44c66d6ed5ff8f6a5ec90fafb0bf8a
asugo <anita.sugo@uon.edu.au>
2019-11-28 20:24:35 +1100

    Cleans up Radial4, adds comments

--- src/mesh/model/radial4.hpp
+++ src/mesh/model/radial4.hpp
@@ -44,141 +44,161 @@ namespace model {
                                                             const Scalar& k,
                                                             const Scalar& max_distance) {
       std::vector<Node<Scalar, N_NEIGHBOURS>> nodes;
+      // Allows adjustment of starting offset in theta for each ring
       std::vector<Scalar> Theta_Offset;
+      // Stores the number of points in each ring
       std::vector<int> number_points;
 
       // L, T, R, B
+      // add easily configurable neighbour ordering
       int LEFT  = 0;
       int TOP   = 1;
       int RIGHT = 2;
       int BELOW = 3;
 
+      // Add origin point to node vector
+      // ?the left and right neighbours may need to be switched, pending experiment.
+      nodes.push_back(Node<Scalar, N_NEIGHBOURS>{{0, 0, -1}, {3, 1, 7, 5}});
 
-      // **********************************DATA*******************************
-
-      std::vector<int> DISTRIBUTION;
-      std::vector<Scalar> THETA_NEXT;
-      // **********************************************************************
-
-      std::array<int, 4> first_neighbours = {2, 3, 0, 1};
-
-      Scalar phi_first = shape.phi(1 / k, h) / 2;
+      // Stores the number of points for the origin patch
+      std::vector<int> origin_number_points;
+      // Add the one point for the origin
+      origin_number_points.emplace_back(1);
+      // Theta offset for first ring
       Theta_Offset.push_back(0);
 
-      for (int j = 0; j < 4; ++j) {
+      // Initiate first ring
+      Scalar phi_first = shape.phi(1 / k, h);
+      // Eight points in the first ring, all connect below to the origin
+      for (int j = 0; j < 8; ++j) {
         Node<Scalar, N_NEIGHBOURS> first;
-        first.ray               = unit_vector(std::sin(phi_first / 2), std::cos(phi_first / 2), j * (2 * M_PI / 4));
-        first.neighbours[BELOW] = first_neighbours[j];
-        first.neighbours[LEFT]  = ((j + 1) % 4 + 4) % 4;
-        first.neighbours[RIGHT] = ((j - 1) % 4 + 4) % 4;
+        first.ray               = unit_vector(std::sin(phi_first), std::cos(phi_first), j * (2 * M_PI / 8));
+        first.neighbours[BELOW] = 0;
+        first.neighbours[LEFT]  = 1 + ((j + 1) % 8 + 8) % 8;
+        first.neighbours[RIGHT] = 1 + ((j - 1) % 8 + 8) % 8;
         nodes.push_back(std::move(first));
       }
-      number_points.emplace_back(4);
-      THETA_NEXT.push_back(2 * M_PI / 4);
-      DISTRIBUTION.push_back(1);
+      number_points.emplace_back(8);
 
+      // Stores the last index in order to loop through the last row to generate the next row
       int running_index = nodes.size();
 
-      std::vector<int> origin_number_points;
-      origin_number_points.emplace_back(0);
-      origin_number_points.emplace_back(4);
-
+      // The variable stopping row for the origin patch
       int stop;
       if (k < 9) { stop = 5; }
       else {
         stop = 8;
       }
 
-      for (int i = 0; i < stop; ++i) {
+      // The number of points in each ring to create the origin patch
+      for (int i = 1; i < stop; ++i) {
         origin_number_points.emplace_back(8 + 8 * i);
       }
 
       bool half_offset = false;
-      for (int v = 1; h * std::tan(shape.phi(v / k, h)) < max_distance; ++v) {
-        // check this int to Scalar conversion
-        Scalar phi_next = shape.phi(v / k, h);
-        int begin       = running_index - number_points.back();
-        int end         = running_index;
-        bool every_one  = false;
-        bool growing    = false;
 
-        // hack // odd v generates clockwise, even v generates anti-clockwise.
-        int one               = std::round(std::pow(-1, v));
+      // Generate the rest of the mesh, v must be int
+      for (int v = 1; h * std::tan(shape.phi(v / k, h)) < max_distance; ++v) {
+        // Calculates the phi value of the ring that is being generated
+        // The int to scalar conversion here will work since k is scalar
+        Scalar phi_next = shape.phi((v + 1) / k, h);
+        // Calculates the index in nodes of the first node on the last ring
+        int begin = running_index - number_points.back();
+        // The index where last ring stops
+        int end = running_index;
+
+        // Avoids consecutive splits
+        bool every_one = false;
+
+        // Handles the addition of only a single point between rings
+        bool growing = false;
+
+        // Odd v generates clockwise, even v generates anti-clockwise
+        int one = (v % 2 == 0 ? 1 : -1);
+        // Number of points in the last ring
         int number_points_now = number_points[v - 1];
+        // Number of points in the next ring
         int number_points_next;
 
-        // precalculate the number of points vector according to distribution and origin patch
-        // fix this limit
+        // Calculate the number of points on the next ring according to dtheta or the origin patch
         if (v < stop) { number_points_next = origin_number_points[v]; }
         else {
           number_points_next = std::ceil((2 * M_PI * k) / shape.theta(phi_next, h));
         }
-
+        // Find the difference between the generating ring and ring to be generated
         int number_difference = number_points_next - number_points_now;
+
+        // dtheta of the next ring
         Scalar theta_next;
 
         // *************** Calculate Split Distribution ***********************
         int distribution;
         if (number_difference == 0) {
-          // std::cout << "Difference is zero: " << std::endl;
+          // Difference is zero
           distribution       = 0;
           number_points_next = number_points_now;
         }
         else if (number_difference == 1) {
-          // std::cout << "Difference is 1: " << std::endl;
+          // Difference is 1
           growing      = true;
           distribution = 1;
           // number_difference = 1;
+          // Although this may be redundant atm I think it is necessary
           number_points_next = 1 + number_points_now;
         }
         else if (number_difference > 1) {
           growing = true;
           if (number_difference < number_points_now) {
-            // std::cout << "Difference is reasonable: " << std::endl;
+            // Difference divides the number of points in the generating ring
             distribution = number_points_now / number_difference;
+            // Difference does not divide the number of points in the generating ring
             if (distribution == 1) { every_one = true; }
           }
           else {
             // number_difference >= number_points_now
-            // std::cout << v << std::endl;
-            // std::cout << "Difference is greater than generating ring: " << std::endl;
+            // Difference is greater than or equal to the number of points in the generating ring
             distribution       = 1;
             number_points_next = 2 * number_points_now;
             number_difference  = number_points_now;
           }
         }
         else {
-          // std::cout << "Difference is negative: " << std::endl;
+          // Difference is negative so maintain constant growth until the number of points is increasing
+          // This case will happen sometimes (sometimes when connecting the origin patch)
           distribution       = 0;
           number_points_next = number_points_now;
         }
         // *********************************************************************************
 
+        // Calculate theta_next instead of dtheta to correct for a change in number of points next based on the
+        // distribution above
         theta_next = 2 * M_PI / number_points_next;
+        // Store the number of points of the next ring
         number_points.emplace_back(number_points_next);
 
-        THETA_NEXT.push_back(theta_next);
-        DISTRIBUTION.push_back(distribution);
-
-        if (distribution >= 2 * k + 2) { half_offset = true; };
-
+        // Gets the global indices of the last row
         std::vector<int> indices;
         for (int i = begin; i < end; ++i) {
           indices.push_back(i);
         }
 
+        // Switches the offset of the splits to half the distribution when the distribution is greater than 2k + 2
+        if (distribution >= 2 * k + 2) { half_offset = true; };
+
         // Chooses starting node
         int new_offset = 1;
         if (v == 1) { new_offset = 0; }
         else if (half_offset) {
+          //?could use ceil
           new_offset = std::floor(distribution / 2);
         }
         else {
           new_offset = 1;
         }
 
+        // This rearranges the indices of the nodes of the last ring according to the reversal of the direction and
+        // to offset of the splits
         std::vector<int> vector_of_indices;
-
         for (int m = new_offset; m >= 0; --m) {
           vector_of_indices.push_back(indices[m]);
         }
@@ -186,13 +206,18 @@ namespace model {
           vector_of_indices.push_back(indices[p]);
         }
 
+        // Initiate relative indices within the ring that will be generated
         int relative_index_now  = 0;
         int relative_index_next = 0;
-        int number_splits       = 0;
+        // Keeps track of number of splits
+        int number_splits = 0;
+        // Gets the theta position of the starting node
         Scalar theta_offset =
           std::atan2(nodes[*vector_of_indices.begin()].ray[1], nodes[*vector_of_indices.begin()].ray[0]);
 
+        // Loops through the nodes of the last ring to generate the new nodes
         for (auto it = vector_of_indices.begin(); it != vector_of_indices.end(); ++it) {
+          // *************** Generate First Node ***********************
           Node<Scalar, N_NEIGHBOURS> new_node;
           new_node.ray =
             unit_vector(std::sin(phi_next), std::cos(phi_next), theta_offset + one * relative_index_next * theta_next);
@@ -203,13 +228,15 @@ namespace model {
             end + (((relative_index_next - one) % number_points_next + number_points_next) % number_points_next);
           new_node.neighbours[BELOW] = *it;
 
-          // nodes[*it].neighbours[TOP] = end + relative_index_next % number_points_next;
+          // Real equation: nodes[*it].neighbours[TOP] = end + relative_index_next % number_points_next;
           nodes[*it].neighbours[TOP] = end + relative_index_next;
 
           nodes.push_back(std::move(new_node));
           relative_index_next += 1;
+          // *********************************************************************************
 
           // *************** Generate Second Node ***********************
+          // Add second node if a split
           if (growing) {
             if (every_one == true) {
               if (number_splits <= number_points_now - number_difference) { distribution = 2; }
@@ -220,7 +247,6 @@ namespace model {
 
             // split every point of according to the distribution until difference is reached, or split every point.
             if ((relative_index_now % distribution == 0 || distribution == 1) && number_splits < number_difference) {
-              // std::cout << "Graph is growing!" << std::endl;
               Node<Scalar, N_NEIGHBOURS> second_new_node;
               second_new_node.ray = unit_vector(
                 std::sin(phi_next), std::cos(phi_next), theta_offset + one * relative_index_next * theta_next);
@@ -243,89 +269,10 @@ namespace model {
         running_index = nodes.size();
       }
 
-      // specify the neighbours of the last ring of points
+      // Join the last ring of points to one past the end
       for (unsigned int i = (nodes.size() - number_points.back()); i < nodes.size(); ++i) {
-        nodes[i].neighbours[TOP] = i;
+        nodes[i].neighbours[TOP] = nodes.size();
       }
-
-      //***********************************Data Collector***************************************
-      std::vector<Scalar> PHI;
-      std::vector<Scalar> THETA_RAW;
-      std::vector<int> NUMBER;
-
-
-      for (int j = 0; h * std::tan(shape.phi(j / k, h)) < max_distance; ++j) {
-        PHI.push_back(shape.phi(j / k, h));
-        THETA_RAW.push_back(shape.theta(shape.phi(j / k, h), h));
-        NUMBER.push_back(std::ceil((2 * M_PI * k) / shape.theta(shape.phi(j / k, h), h)));
-      }
-
-      int last;
-      last = std::max(
-        {PHI.size(), THETA_RAW.size(), NUMBER.size(), number_points.size(), THETA_NEXT.size(), DISTRIBUTION.size()});
-
-      // if (h >= 0.94 - 0.001 && h <= 0.94 + 0.001) {
-      //   std::ofstream outfile;
-      //   outfile.open("/home/asugo/LatexPlotData/Error/Robot6.csv");
-      //   if (outfile.fail()) { std::cout << "Couldn't open the file!" << std::endl; }
-      //   // outfile << "#,"
-      //   // << "kNumber," << k << ",Height," << h << ",Stop," << stop << ",LastRing," << last << ",Distance,"
-      //   // << max_distance << "\n";
-      //   // outfile << "PhiNumber,Phi,THETA_RAW,NumberOfPoints,PointsByGraph,Difference,\n";
-      //   outfile
-      //     <<
-      //     "PhiNumber,Phi,Distance,Theta_Raw,NumberOfPoints,PointsByGraph,Difference,Distribution,Theta_next,Ratio\n";
-      //   int diff;
-      //   for (size_t j = 5; j < last; ++j) {
-      //     if (std::isfinite(THETA_RAW[j]) && std::isfinite(THETA_RAW[j - 1])) { diff = NUMBER[j] - NUMBER[j - 1]; }
-      //     else {
-      //       diff = 0;
-      //     }
-      //     Scalar distance = h * std::tan(PHI[j]);
-      //     Scalar T        = std::isfinite(THETA_RAW[j]) ? THETA_RAW[j] : 10000;
-      //     Scalar N        = std::isfinite(THETA_RAW[j]) ? NUMBER[j] : 0;
-      //     int Ratio       = std::floor(THETA_NEXT[j] / (THETA_NEXT[j - 1] - THETA_NEXT[j]));
-      //     outfile << j << "," << PHI[j] << "," << distance << "," << T << "," << N << "," << number_points[j] << ","
-      //             << diff << "," << DISTRIBUTION[j] << "," << THETA_NEXT[j] << "," << Ratio << "\n";
-      //   }
-      //   outfile.close();
-      // }
-
-
-      if (h >= 0.94 - 0.001 && h <= 0.94 + 0.001) {
-
-        std::vector<std::vector<Scalar>> Variation;
-
-        for (int i = 1; i < 4; ++i) {
-          // Scalar counter = nodes.size() - 1 - (1000 * i);
-          Scalar counter = nodes.size() - 1 - 4000 + (900 * i);
-          std::cout << counter << std::endl;
-          std::vector<Scalar> variation;
-          for (int t = last - 1; t > 2; --t) {
-            variation.push_back(std::atan2(nodes[counter].ray[1], nodes[counter].ray[0]));
-            counter = nodes[counter].neighbours[BELOW];
-          }
-          Variation.push_back(variation);
-        }
-
-
-        std::ofstream outfile2;
-        outfile2.open("/home/asugo/LatexPlotData/ThetaVariationQuadAlt8.csv");
-        outfile2 << "Phi_Number,Distance,Theta0,Theta1,Theta2\n";
-
-        for (unsigned int t = 0; t < Variation[0].size(); ++t) {
-          outfile2 << last - 1 - t << "," << h * std::tan(PHI[last - 1 - t]) << "," << Variation[0][t] << ","
-                   << Variation[1][t] << "," << Variation[2][t] << "\n";
-        }
-      }
-
-      //****************************************************************************************
-
-      // // print out mesh points
-      // for (int i = 0; i < nodes.size(); ++i) {
-      //   std::cout << "meshpoint: " << i << ": " << nodes[i].neighbours[LEFT] << ", " << nodes[i].neighbours[RIGHT]
-      //             << ", " << nodes[i].neighbours[TOP] << ", " << nodes[i].neighbours[BELOW] << std::endl;
-      // }
 
       return nodes;
     }
--- src/visualmesh.hpp
+++ src/visualmesh.hpp
@@ -25,6 +25,7 @@
 
 #include "engine/cpu/cpu_engine.hpp"
 #include "mesh/mesh.hpp"
+#include "mesh/model/radial4.hpp"
 #include "mesh/model/ring6.hpp"
 
 namespace visualmesh {
@@ -41,7 +42,7 @@ namespace visualmesh {
  */
 template <typename Scalar                  = float,
           template <typename> class Engine = engine::cpu::Engine,
-          template <typename> class Model  = model::Ring6>
+          template <typename> class Model  = model::Radial4>
 class VisualMesh {
 public:
   /**
