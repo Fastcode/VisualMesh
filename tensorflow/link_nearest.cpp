14c8ca81c37612aa974b88c40707e390a20c74ad
Alex Biddulph <c3124185@uon.edu.au>
2020-06-30 18:34:07 +1000

    Refactor link_nearest op

--- tensorflow/link_nearest.cpp
+++ tensorflow/link_nearest.cpp
@@ -27,8 +27,6 @@
 #include "utility/phi_difference.hpp"
 
 template <typename T>
-using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
-template <typename T>
 using Tensor = Eigen::Tensor<T, 2, Eigen::RowMajor>;
 
 enum Args {
@@ -64,30 +62,125 @@ REGISTER_OP("LinkNearest")
   });
 
 /**
- * @brief The Align Graphs tensorflow op
+ * @brief The Link Nearest tensorflow op
  *
  * @details
- *   This op with search through both graphs finding the nodes that are closest to the nodes in the opposite graph.
+ *   For each vector in v_a find the vector in v_b that is radially closest to it.
  *
- *   Given a node from the right graph, a search is made for the node in the left graph that is closest to it. This
- * search is repeated for every node in the right graph, generating a list of indices linking each node in the right
- * graph to its match in the left graph.
+ *   Vectors in v_a are first transformed so that they are viewed from the same perspective as the vectors in v_b.
  *
- *   The same search is performed for nodes in the left graph, the resulting index list linking nodes in the left graph
- * to their match in the right graph.
+ *   A graph search through g_b is then performed searching for the vector in v_b that has the smallest radial distance
+ * to any given vector in v_a. If the shortest distance to all vectors in v_b exceeds a specified distance_threshold
+ * then the vector in v_a is deemed to be offscreen.
  *
  * @tparam T The scalar type used for floating point numbers
  * @tparam U The scalar type used for integer numbers
  */
 template <typename T, typename U>
 class LinkNearestOp : public ShapeOpBase<T, LinkNearestOp<T, U>, Args::GEOMETRY, Args::RADIUS> {
+private:
+    template <typename Shape>
+    // Calculate radial distance between the two nodes
+    T radial_distance(const Eigen::Matrix<T, 3, 1>& a,
+                      const Eigen::Matrix<T, 3, 1>& b,
+                      const Eigen::Matrix<T, 4, 4>& Hoc,
+                      const Shape& shape) {
+        auto r_d = visualmesh::util::phi_difference(Hoc(2, 3), shape.c(), {a.x(), a.y(), a.z()}, {b.x(), b.y(), b.z()});
+        return std::abs(shape.n(r_d.phi_0, r_d.h_prime) - shape.n(r_d.phi_1, r_d.h_prime));
+    }
+
+    // Given a node index check the distance of each of its neighbours to a given reference vector
+    template <typename Shape>
+    std::pair<U, T> check_neighbours(const U& index,
+                                     const Eigen::Matrix<T, 3, 1>& reference,
+                                     const Eigen::Matrix<T, Eigen::Dynamic, 3>& pool,
+                                     const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& neighbours,
+                                     const Eigen::Matrix<T, 4, 4>& Hoc,
+                                     const Shape& shape) {
+        // Get distance to the first neighbour
+        U min_index    = neighbours(index, 0);
+        T min_distance = radial_distance(pool.row(min_index), reference, Hoc, shape);
+
+        // Now find the minumum distance over all neighbours
+        for (U neighbour_index = 1; neighbour_index < neighbours.cols(); ++neighbour_index) {
+            U new_index                      = neighbours(index, neighbour_index);
+            Eigen::Matrix<T, 3, 1> neighbour = pool.row(new_index).transpose();
+
+            T new_distance = radial_distance(neighbour, reference, Hoc, shape);
+
+            if (new_distance < min_distance) {
+                min_index    = new_index;
+                min_distance = new_distance;
+            }
+        }
+
+        // Return the node index and distance for the neighbour with the smallest distance
+        return std::make_pair(min_index, min_distance);
+    }
+
+    // For each reference vector find the index to the vector in the pool that is closest to it
+    template <typename Shape>
+    Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic> match_vectors(
+      const Eigen::Matrix<T, Eigen::Dynamic, 3>& references,
+      const Eigen::Matrix<T, Eigen::Dynamic, 3>& pool,
+      const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& neighbours,
+      const Eigen::Matrix<T, 4, 4>& Hoc,
+      const Shape& shape,
+      const T& distance_threshold) {
+        Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic> matches =
+          Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic>::Zero(references.rows(), 1);
+
+        for (U reference_index = 0; reference_index < references.rows(); ++reference_index) {
+            // Extract the reference vector
+            const Eigen::Matrix<T, 3, 1> reference = references.row(reference_index).transpose();
+
+            // Calculate pool vector starting index (hopefully this index is close to the final index)
+            U min_index = reference_index * pool.rows() / references.rows();
+
+            // Set initial distance as the distance between the reference vector and selected pool vector
+            T min_distance = radial_distance(pool.row(min_index), reference, Hoc, shape);
+
+            // Check distance to the neighbours of the selected index in the pool to find the neighbour to
+            // start the graph search from.
+            U new_min_index;
+            T new_min_distance;
+            std::tie(new_min_index, new_min_distance) =
+              check_neighbours(min_index, reference, pool, neighbours, Hoc, shape);
+
+            // Start checking the rest of the graph
+            // Find the neighbour of the current index that is closest to the reference vector
+            // If none of the neighbours are closer to the reference vector than the current pool vector
+            // then our search is over.
+            while (new_min_distance < min_distance) {
+                min_distance = new_min_distance;
+                min_index    = new_min_index;
+                std::tie(new_min_index, new_min_distance) =
+                  check_neighbours(min_index, reference, pool, neighbours, Hoc, shape);
+            }
+
+            // Check to see if the closest point is offscreen.
+            if (new_min_distance > distance_threshold) { min_index = -std::numeric_limits<U>::max(); }
+            else {
+                min_index = new_min_index;
+            }
+
+            // min_index is now the index of the pool vector that is closest to the current reference vector
+            matches(reference_index) = min_index;
+        }
+        return matches;
+    }
+
 public:
     explicit LinkNearestOp(tensorflow::OpKernelConstruction* context)
       : ShapeOpBase<T, LinkNearestOp<T, U>, Args::GEOMETRY, Args::RADIUS>(context) {}
 
     template <typename Shape>
     void DoCompute(tensorflow::OpKernelContext* context, const Shape& shape) {
-        // Check that the shape of each of the inputs is valid
+
+        /***********************************************************
+         *** Check that the shape of each of the inputs is valid ***
+         ***********************************************************/
+
         OP_REQUIRES(context,
                     tensorflow::TensorShapeUtils::IsMatrix(context->input(Args::V_A).shape())
                       && context->input(Args::V_A).shape().dim_size(1) == 3,
@@ -111,27 +204,44 @@ public:
                     tensorflow::TensorShapeUtils::IsScalar(context->input(Args::THRESHOLD).shape()),
                     tensorflow::errors::InvalidArgument("Distance threshold must be a scalar"));
 
-        // Extract information from our input tensors
+
+        /**************************************************
+         *** Extract information from our input tensors ***
+         **************************************************/
+
         T distance_threshold      = context->input(Args::THRESHOLD).scalar<T>()(0);
         tensorflow::Tensor tV_a   = context->input(Args::V_A);
         tensorflow::Tensor tV_b   = context->input(Args::V_B);
         tensorflow::Tensor tG_b   = context->input(Args::G_B);
         tensorflow::Tensor tHoc_a = context->input(Args::HOC_A);
         tensorflow::Tensor tHoc_b = context->input(Args::HOC_B);
-        Matrix<T> v_a =
-          Eigen::Map<Matrix<T>>(tV_a.tensor<T, 2>().data(), tV_a.shape().dim_size(0), tV_a.shape().dim_size(1));
-        Matrix<T> v_b =
-          Eigen::Map<Matrix<T>>(tV_b.tensor<T, 2>().data(), tV_b.shape().dim_size(0), tV_b.shape().dim_size(1));
-        Matrix<T> g_b =
-          Eigen::Map<Matrix<T>>(tG_b.tensor<T, 2>().data(), tG_b.shape().dim_size(0), tG_b.shape().dim_size(1));
-        Matrix<T> Hoc_a =
-          Eigen::Map<Matrix<T>>(tHoc_a.tensor<T, 2>().data(), tHoc_a.shape().dim_size(0), tHoc_a.shape().dim_size(1));
-        Matrix<T> Hoc_b =
-          Eigen::Map<Matrix<T>>(tHoc_b.tensor<T, 2>().data(), tHoc_b.shape().dim_size(0), tHoc_b.shape().dim_size(1));
 
 
-        // Project left and right vectors on to the observation plane
-        v_a = (v_a * (Hoc_a(2, 3) - shape.c())).cwiseQuotient(v_a.template rightCols<1>());
+        /****************************************************
+         *** Convert TensorFlow tensors to Eigen Matrices ***
+         ****************************************************/
+
+        Eigen::Matrix<T, Eigen::Dynamic, 3> v_a =
+          Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, 3>>(tV_a.tensor<T, 2>().data(), tV_a.shape().dim_size(0), 3);
+        Eigen::Matrix<T, Eigen::Dynamic, 3> v_b =
+          Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, 3>>(tV_b.tensor<T, 2>().data(), tV_b.shape().dim_size(0), 3);
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> g_b =
+          Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>(
+            tG_b.tensor<T, 2>().data(), tG_b.shape().dim_size(0), tG_b.shape().dim_size(1));
+        Eigen::Matrix<T, 4, 4> Hoc_a = Eigen::Map<Eigen::Matrix<T, 4, 4>>(
+          tHoc_a.tensor<T, 2>().data(), tHoc_a.shape().dim_size(0), tHoc_a.shape().dim_size(1));
+        Eigen::Matrix<T, 4, 4> Hoc_b = Eigen::Map<Eigen::Matrix<T, 4, 4>>(
+          tHoc_b.tensor<T, 2>().data(), tHoc_b.shape().dim_size(0), tHoc_b.shape().dim_size(1));
+
+
+        /****************************************
+         *** Project v_a into the view of v_b ***
+         ****************************************/
+
+
+        // Project v_a on to the observation plane
+        v_a = v_a * (Hoc_a(2, 3) - shape.c());
+        v_a = (v_a.array().colwise() / v_a.template rightCols<1>().array()).matrix();
 
         // Translate left vectors to right view and right vectors to left view and renormalise
         // Points:
@@ -141,90 +251,19 @@ public:
         //    N - projected node position
         // Spaces:
         //    o - observation plane origin
-        const Eigen::Matrix<T, 3, 1> rAOo(Hoc_a(0, 3), Hoc_a(1, 3), Hoc_a(2, 3));
-        const Eigen::Matrix<T, 3, 1> rBOo(Hoc_b(0, 3), Hoc_b(1, 3), Hoc_b(2, 3));
-        const Eigen::Matrix<T, 3, 1> rABo = rAOo - rBOo;
+        const Eigen::Matrix<T, 1, 3> rAOo(Hoc_a(0, 3), Hoc_a(1, 3), Hoc_a(2, 3));
+        const Eigen::Matrix<T, 1, 3> rBOo(Hoc_b(0, 3), Hoc_b(1, 3), Hoc_b(2, 3));
+        const Eigen::Matrix<T, 1, 3> rABo = rAOo - rBOo;
 
         // These are the A vectors projected into the B camera
         // rNBo = rNAo + rABo
-        Matrix<T> rNBo = (v_a + rABo).normalized();
+        Eigen::Matrix<T, Eigen::Dynamic, 3> rNBo = (v_a.rowwise() + rABo).rowwise().normalized();
 
-        // Calculate radial distance between the two nodes
-        auto radial_distance = [&](const Eigen::Matrix<T, 3, 1>& a, const Eigen::Matrix<T, 3, 1>& b) -> T {
-            auto r_d =
-              visualmesh::util::phi_difference(Hoc_b(2, 3), shape.c(), {a.x(), a.y(), a.z()}, {b.x(), b.y(), b.z()});
-            return std::abs(shape.n(r_d.phi_0, r_d.h_prime) - shape.n(r_d.phi_1, r_d.h_prime));
-        };
 
-        // Given a node index check the distance of each of its neighbours to a given reference vector
-        auto check_neighbours = [&](const U& index,
-                                    const Matrix<T>& vectors,
-                                    const Matrix<T>& reference,
-                                    const Matrix<T>& neighbours) -> std::pair<U, T> {
-            // Get distance to the first neighbour
-            U min_index    = neighbours(index, 0);
-            T min_distance = radial_distance(vectors.row(min_index), reference);
+        /**********************
+         *** Compute output ***
+         **********************/
 
-            // Now find the minumum distance over all neighbours
-            for (U neighbour_index = 1; neighbour_index < neighbours.cols(); ++neighbour_index) {
-                U new_index         = neighbours(index, neighbour_index);
-                Matrix<T> neighbour = vectors.row(new_index);
-
-                T new_distance = radial_distance(neighbour, reference);
-
-                if (new_distance < min_distance) {
-                    min_index    = new_index;
-                    min_distance = new_distance;
-                }
-            }
-
-            // Return the node index and distance for the neighbour with the smallest distance
-            return std::make_pair(min_index, min_distance);
-        };
-
-        // For each reference vector find the index to the vector in the pool that is closest to it
-        auto match_vectors =
-          [&](const Matrix<T>& pool, const Matrix<T>& references, const Matrix<T>& neighbours) -> Matrix<U> {
-            Matrix<U> matches = Matrix<U>::Zero(references.rows(), 1);
-
-            for (U reference_index = 0; reference_index < references.rows(); ++reference_index) {
-                const Matrix<T>& reference = references.row(reference_index);
-
-                // Calculate pool vector starting index (hopefully this index is close to the final index)
-                U min_index = reference_index * pool.rows() / references.rows();
-
-                // Set initial distance as the distance between the reference vector and selected pool vector
-                T min_distance = radial_distance(pool.row(min_index), reference);
-
-                // Check distance to the neighbours of the selected index in the pool to find the neighbour to
-                // start the graph search from.
-                U new_min_index;
-                T new_min_distance;
-                std::tie(new_min_index, new_min_distance) = check_neighbours(min_index, pool, reference, neighbours);
-
-                // Start checking the rest of the graph
-                // Find the neighbour of the current index that is closest to the reference vector
-                // If none of the neighbours are closer to the reference vector than the current pool vector
-                // then our search is over.
-                while (new_min_distance < min_distance) {
-                    min_distance = new_min_distance;
-                    min_index    = new_min_index;
-                    std::tie(new_min_index, new_min_distance) =
-                      check_neighbours(min_index, pool, reference, neighbours);
-                }
-
-                // Check to see if the closest point is offscreen.
-                if (new_min_distance > distance_threshold) { min_index = -std::numeric_limits<U>::max(); }
-                else {
-                    min_index = new_min_index;
-                }
-
-                // min_index is now the index of the pool vector that is closest to the current reference
-                // vector
-                matches(reference_index) = min_index;
-            }
-            return matches;
-        };
 
         // Create the output matrix to hold the matching indices
         tensorflow::Tensor* matches = nullptr;
@@ -232,7 +271,8 @@ public:
 
         // Find the matching indices for each vector in the right view
         matches->matrix<U>() = Eigen::TensorMap<Tensor<U>, Eigen::Aligned>(
-          match_vectors(v_a, v_b, g_b).data(), {tV_b.shape().dim_size(0), tV_b.shape().dim_size(1)});
+          match_vectors(rNBo, v_b, g_b, Hoc_b, shape, distance_threshold).data(),
+          {tV_b.shape().dim_size(0), tV_b.shape().dim_size(1)});
     }
 };
 
