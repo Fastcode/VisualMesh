3aef1fbd7709b0e8baac2596a92770d36f5dbdd3
Kip Hamiltons <p0nage@live.com.au>
2021-05-01 17:30:03 +1000

    Minimum viable product complete

---
+++ mesh_check/dataset.hpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2017-2020 Trent Houliston <trent@houliston.me>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef EXAMPLE_DATASET_HPP
+#define EXAMPLE_DATASET_HPP
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <yaml-cpp/yaml.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <utility>
+#include <vector>
+
+#include "visualmesh/lens.hpp"
+#include "visualmesh/utility/math.hpp"
+
+template <typename Scalar>
+struct dataset_element {
+    std::string number;
+    cv::Mat image;
+    visualmesh::Lens<Scalar> lens;
+    visualmesh::mat4<Scalar> Hoc;
+};
+
+template <typename Scalar>
+std::vector<dataset_element<Scalar>> load_dataset(const std::string& path) {
+
+    auto dir = ::opendir(path.c_str());
+
+    if (dir != nullptr) {
+        std::vector<dataset_element<Scalar>> dataset;
+        for (dirent* ent = readdir(dir); ent != nullptr; ent = readdir(dir)) {
+            auto file = std::string(ent->d_name);
+
+            if (file.substr(0, 4) == "lens" && !(ent->d_type & DT_DIR)) {
+
+                dataset_element<Scalar> element;
+
+                // Extract the number so we can find the other files
+                element.number         = file.substr(4, 7);
+                element.image          = cv::imread(path + "/image" + element.number + ".jpg");
+                YAML::Node lens        = YAML::LoadFile(path + "/" + file);
+                std::string projection = lens["projection"].as<std::string>();
+
+                // Convert the image from BGR to BGRA
+                cv::cvtColor(element.image, element.image, cv::COLOR_BGR2BGRA);
+
+                // Load the lens parameters
+                element.lens.projection = projection == "RECTILINEAR"
+                                            ? visualmesh::RECTILINEAR
+                                            : projection == "EQUIDISTANT"
+                                                ? visualmesh::EQUIDISTANT
+                                                : projection == "EQUISOLID"
+                                                    ? visualmesh::EQUISOLID
+                                                    : static_cast<visualmesh::LensProjection>(-1);
+
+                element.lens.dimensions   = {element.image.cols, element.image.rows};
+                element.lens.focal_length = lens["focal_length"].as<Scalar>();
+                element.lens.centre       = lens["centre"].as<visualmesh::vec2<Scalar>>();
+                element.lens.k            = lens["k"].as<visualmesh::vec2<Scalar>>();
+                element.lens.fov          = lens["fov"].as<Scalar>();
+
+                // Load the Hoc matrix
+                element.Hoc = lens["Hoc"].as<visualmesh::mat4<Scalar>>();
+
+                dataset.push_back(element);
+            }
+        }
+        ::closedir(dir);
+        return dataset;
+    }
+    else {
+        throw std::system_error(errno, std::system_category(), "Failed to open directory " + path);
+    }
+}
+
+#endif  // EXAMPLE_DATASET_HPP
---
+++ mesh_check/mesh_types.cpp
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2017-2020 Trent Houliston <trent@houliston.me>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <array>
+#include <map>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <string>
+#include <vector>
+
+#include "Timer.hpp"
+#include "dataset.hpp"
+#include "draw.hpp"
+#include "visualmesh/geometry/Sphere.hpp"
+#include "visualmesh/model/nmgrid4.hpp"
+#include "visualmesh/model/nmgrid6.hpp"
+#include "visualmesh/model/nmgrid8.hpp"
+#include "visualmesh/model/radial4.hpp"
+#include "visualmesh/model/radial6.hpp"
+#include "visualmesh/model/radial8.hpp"
+#include "visualmesh/model/ring4.hpp"
+#include "visualmesh/model/ring6.hpp"
+#include "visualmesh/model/ring8.hpp"
+#include "visualmesh/model/xmgrid4.hpp"
+#include "visualmesh/model/xmgrid6.hpp"
+#include "visualmesh/model/xmgrid8.hpp"
+#include "visualmesh/model/xygrid4.hpp"
+#include "visualmesh/model/xygrid6.hpp"
+#include "visualmesh/model/xygrid8.hpp"
+#include "visualmesh/visualmesh.hpp"
+
+using Scalar = float;
+
+// Use OpenCL engine if available, then Vulkan then CPU
+#if defined(VISUALMESH_ENABLE_OPENCL)
+
+#include "visualmesh/engine/opencl/engine.hpp"
+template <typename Scalar>
+using Engine = visualmesh::engine::opencl::Engine<Scalar>;
+
+#elif defined(VISUALMESH_ENABLE_VULKAN)
+
+#include "visualmesh/engine/opencl/engine.hpp"
+template <typename Scalar>
+using Engine = visualmesh::engine::vulkan::Engine<Scalar, false>;
+
+#else
+
+#include "visualmesh/engine/cpu/engine.hpp"
+template <typename Scalar>
+using Engine = visualmesh::engine::cpu::Engine<Scalar>;
+
+#endif
+
+int main() {
+    std::string image_path = "../example/images";
+
+    // Create windows
+    cv::namedWindow("Radial 4", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Radial 6", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Radial 8", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Ring 4", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Ring 6", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Ring 8", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("XM Grid 4", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("XM Grid 6", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("XM Grid 8", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("XY Grid 4", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("XY Grid 6", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("XY Grid 8", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("NM Grid 4", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("NM Grid 6", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("NM Grid 8", cv::WINDOW_AUTOSIZE);
+
+    visualmesh::geometry::Sphere<Scalar> sphere(0.0949996);
+
+    // Build meshes
+    Timer t;
+    visualmesh::VisualMesh<Scalar, visualmesh::model::Radial4> radial4(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built Radial 4");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::Radial6> radial6(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built Radial 6");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::Radial8> radial8(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built Radial 8");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::Ring4> ring4(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built Ring 4");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::Ring6> ring6(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built Ring 6");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::Ring8> ring8(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built Ring 8");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::XMGrid4> xmgrid4(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built XM Grid 4");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::XMGrid6> xmgrid6(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built XM Grid 6");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::XMGrid8> xmgrid8(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built XM Grid 8");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::XYGrid4> xygrid4(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built XY Grid 4");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::XYGrid6> xygrid6(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built XY Grid 6");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::XYGrid8> xygrid8(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built XY Grid 8");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::NMGrid4> nmgrid4(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built NM Grid 4");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::NMGrid6> nmgrid6(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built NM Grid 6");
+    visualmesh::VisualMesh<Scalar, visualmesh::model::NMGrid8> nmgrid8(sphere, 0.5, 1.5, 4, 0.5, 20);
+    t.measure("Built NM Grid 8");
+
+    // Build engines
+    Engine<Scalar> engine;
+
+    // Load dataset
+    auto dataset = load_dataset<Scalar>(image_path);
+
+    for (const auto& element : dataset) {
+        draw("Radial 4", element.image, engine(radial4, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("Radial 6", element.image, engine(radial6, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("Radial 8", element.image, engine(radial8, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("Ring 4", element.image, engine(ring4, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("Ring 6", element.image, engine(ring6, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("Ring 8", element.image, engine(ring8, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("XM Grid 4", element.image, engine(xmgrid4, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("XM Grid 6", element.image, engine(xmgrid6, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("XM Grid 8", element.image, engine(xmgrid8, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("XY Grid 4", element.image, engine(xygrid4, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("XY Grid 6", element.image, engine(xygrid6, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("XY Grid 8", element.image, engine(xygrid8, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("NM Grid 4", element.image, engine(nmgrid4, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("NM Grid 6", element.image, engine(nmgrid6, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        draw("NM Grid 8", element.image, engine(nmgrid8, element.Hoc, element.lens), cv::Scalar(255, 255, 255));
+        if (char(cv::waitKey(0)) == 27) break;
+    }
+
+    // Run through the images doing projections
+}
