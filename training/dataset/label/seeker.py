06237add34a695063834d324dcf688d5dc467547
Trent Houliston <trent@houliston.me>
2021-05-11 11:49:11 +1000

    Upgrade the seeker labeller to handle more cases * Handle the case where no labels exist (predict random far away points) * Add a ratio limit so that if a distance seeker is too far from our observation plane it will be ignored

--- training/dataset/label/seeker.py
+++ training/dataset/label/seeker.py
@@ -13,19 +13,23 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import math
+
 import tensorflow as tf
 from training.op import difference_visual_mesh, map_visual_mesh, unmap_visual_mesh
 from training.projection import project
 
 
 class Seeker:
-    def __init__(self, scale, mesh, geometry, **config):
+    def __init__(self, scale, ratio, mesh, geometry, **config):
         self.scale = scale
 
         # Grab our relevant fields
         self.mesh_model = mesh["model"]
         self.geometry = tf.constant(geometry["shape"], dtype=tf.string, name="GeometryType")
         self.radius = geometry["radius"]
+        self.min_ratio = ratio[0]
+        self.max_ratio = ratio[1]
 
     def features(self):
         return {
@@ -35,14 +39,32 @@ class Seeker:
     def __call__(self, image, Hoc, V, valid, **features):
 
         targets = features["seeker/targets"]
+        projection = features["lens/projection"]
+        focal_length = features["lens/focal_length"]
+        centre = features["lens/centre"]
+        k = features["lens/k"]
+        dims = tf.shape(image)[:2]
 
-        # Transform our targets into observation plane space
-        uOCo, _ = tf.linalg.normalize(tf.einsum("ij,kj->ki", Hoc[:3, :3], targets), axis=-1)
+        # If a target isn't on screen we will be expecting the network to predict on things it can't see
+        # This will result in lower performance as it tries to learn how to do this for the training dataset
+        px = tf.cast(tf.round(project(targets, dims, projection, focal_length, centre, k)), tf.int32)
+        on_screen = tf.reduce_all(tf.logical_and(px >= 0, px < tf.expand_dims(dims, 0)), axis=-1)
+        targets = tf.gather(targets, tf.squeeze(tf.where(on_screen), axis=-1))
+
+        # Transform our remaining targets into observation plane space
+        uOCo, l = tf.linalg.normalize(tf.einsum("ij,kj->ki", Hoc[:3, :3], targets), axis=-1)
+
+        # Check if this signal is close enough in distance to our observation plane to be considered
+        height = Hoc[2, 3]
+        ratio = tf.squeeze(l, axis=-1) / height
+        uOCo = tf.gather(
+            uOCo, tf.squeeze(tf.where(tf.logical_and(ratio > self.min_ratio, ratio < self.max_ratio)), axis=-1)
+        )
 
         # Remove any points that are above the camera in observation plane space as they can never be projected down
         uOCo = tf.gather(uOCo, tf.squeeze(tf.where(uOCo[:, 2] < 0), axis=-1))
 
-        args = {"model": self.mesh_model, "height": Hoc[2, 3], "geometry": self.geometry, "radius": self.radius}
+        args = {"model": self.mesh_model, "height": height, "geometry": self.geometry, "radius": self.radius}
 
         # Get our vectors in nm coordinates
         mesh_nm = unmap_visual_mesh(V, **args)
@@ -60,25 +82,12 @@ class Seeker:
         # Divide distance by scale so a value from 0->scale becomes 0->1
         distance = diff / self.scale
 
-        # Work out if any of the points are on screen so we can throw out samples which we can't see
-        px = tf.cast(
-            tf.round(
-                project(
-                    targets,
-                    tf.shape(image)[:2],
-                    features["lens/projection"],
-                    features["lens/focal_length"],
-                    features["lens/centre"],
-                    features["lens/k"],
-                )
-            ),
-            tf.int32,
-        )
-        on_screen = tf.reduce_any(
-            tf.logical_and(
-                tf.logical_and(px[:, 0] >= 0, px[:, 0] < tf.shape(image)[0]),
-                tf.logical_and(px[:, 1] >= 0, px[:, 1] < tf.shape(image)[1]),
-            )
-        )
+        # If we have no points after doing all this, add a random point
+        # We can use it to ensure that the network learns how to predict when there are no points
+        def random_distance():
+            theta = tf.random.uniform([tf.shape(distance)[0], 1], -math.pi, math.pi)
+            return math.sqrt(2.0) * tf.stack([tf.cos(theta), tf.sin(theta)], axis=-1)
 
-        return {"Y": distance, "valid": tf.logical_and(tf.logical_and(valid, on_screen), tf.shape(uOCo)[0] > 0)}
+        distance = tf.cond(tf.size(distance) > 0, lambda: distance, random_distance)
+
+        return {"Y": distance}
