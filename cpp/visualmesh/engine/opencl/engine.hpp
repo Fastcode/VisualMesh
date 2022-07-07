282fd2192d83dd5303b26353b308234690955b85
Ysobel <ysobelsims@gmail.com>
2022-07-07 10:36:40 +1000

    .

--- cpp/visualmesh/engine/opencl/engine.hpp
+++ cpp/visualmesh/engine/opencl/engine.hpp
@@ -178,7 +178,9 @@ namespace engine {
                     std::cout << "binary size: " << binary_size << " vector size: " << binary_save.size() << std::endl;
                     clGetProgramInfo(program, CL_PROGRAM_BINARIES, binary_save.size(), binary_save.data(), nullptr);
                     std::cout << "binary size: " << binary_size << " vector size: " << binary_save.size() << std::endl;
-
+                    for (auto& b : binary_save) {
+                        std::cout << b;
+                    }
                     std::ofstream write_binary(binary_path, std::ofstream::binary);
                     write_binary.write(binary_save.data(), binary_save.size());
                     write_binary.close();
