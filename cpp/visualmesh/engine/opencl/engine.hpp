afcf39c4e4c3f3369cb9101c811edcfa30713614
Ysobel <ysobelsims@gmail.com>
2022-07-07 11:03:40 +1000

    .

--- cpp/visualmesh/engine/opencl/engine.hpp
+++ cpp/visualmesh/engine/opencl/engine.hpp
@@ -174,16 +174,15 @@ namespace engine {
                     std::cout << "created binary" << std::endl;
                     // Save the the built program to a file
                     clGetProgramInfo(program, CL_PROGRAM_BINARY_SIZES, sizeof(size_t), &binary_size, nullptr);
-                    std::shared_ptr<std::vector<char>> binary_save =
-                      std::make_shared<std::vector<char>>(binary_size, 0);
-                    std::cout << "binary size: " << binary_size << " vector size: " << binary_save->size() << std::endl;
-                    clGetProgramInfo(program, CL_PROGRAM_BINARIES, binary_save->size(), binary_save->data(), nullptr);
-                    std::cout << "binary size: " << binary_size << " vector size: " << binary_save->size() << std::endl;
+                    std::vector<char> binary_save = std::vector<char>(binary_size, 0);
+                    std::cout << "binary size: " << binary_size << " vector size: " << binary_save.size() << std::endl;
+                    clGetProgramInfo(program, CL_PROGRAM_BINARIES, binary_save.size(), &(binary_save.data()), nullptr);
+                    std::cout << "binary size: " << binary_size << " vector size: " << binary_save.size() << std::endl;
                     for (auto& b : *binary_save) {
                         std::cout << b;
                     }
                     std::ofstream write_binary(binary_path, std::ofstream::binary);
-                    write_binary.write(binary_save->data(), binary_save->size());
+                    write_binary.write(binary_save.data(), binary_save.size());
                     write_binary.close();
                     std::cout << "saved binary" << std::endl;
                 }
