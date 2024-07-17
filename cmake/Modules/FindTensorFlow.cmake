d227677d3f1b3f14fff1027776f567c1bc7ed45f
Trent Houliston <trent@houliston.me>
2024-07-17 14:50:30 +0200

    Apply cmake-format to all cmake files (#17)
    cmake-format was not matching what cmake format does now, updates so
    that future PRs don't show this as a diff

--- cmake/Modules/FindTensorFlow.cmake
+++ cmake/Modules/FindTensorFlow.cmake
@@ -19,10 +19,14 @@ execute_process(
     COMMAND ${PYTHON_EXECUTABLE} -c "import tensorflow; print(tensorflow.__version__)"
     OUTPUT_VARIABLE TENSORFLOW_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import tensorflow; print(tensorflow.sysconfig.get_include())"
-                OUTPUT_VARIABLE tf_inc_dir OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import tensorflow; print(tensorflow.sysconfig.get_lib())"
-                OUTPUT_VARIABLE tf_lib_dir OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(
+    COMMAND ${PYTHON_EXECUTABLE} -c "import tensorflow; print(tensorflow.sysconfig.get_include())"
+    OUTPUT_VARIABLE tf_inc_dir
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(
+    COMMAND ${PYTHON_EXECUTABLE} -c "import tensorflow; print(tensorflow.sysconfig.get_lib())"
+    OUTPUT_VARIABLE tf_lib_dir
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 find_path(
     TENSORFLOW_INCLUDE_DIRS
@@ -39,11 +43,6 @@ find_library(
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     TensorFlow
-    FOUND_VAR
-    TensorFlow_FOUND
-    REQUIRED_VARS
-    TENSORFLOW_INCLUDE_DIRS
-    TENSORFLOW_LIBRARIES
-    TENSORFLOW_VERSION
-    VERSION_VAR
-    TENSORFLOW_VERSION)
+    FOUND_VAR TensorFlow_FOUND
+    REQUIRED_VARS TENSORFLOW_INCLUDE_DIRS TENSORFLOW_LIBRARIES TENSORFLOW_VERSION
+    VERSION_VAR TENSORFLOW_VERSION)
